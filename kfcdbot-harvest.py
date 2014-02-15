27ce7d366da52c44aec57165dccde38af7cdb18a
Nick Avgerinos <nicka@axcella.com>
2014-02-15 14:57:26 -0800

    mostly working

--- kfcdbot-harvest.py
+++ kfcdbot-harvest.py
@@ -1,37 +1,56 @@
+import sys
 import os
 import twitter
 
 LASTID_FILE='.lastid'
-CORPUS_FILE="kftweets.txt"
+
+if len(sys.argv) < 2:
+    print """
+   %s - Python Twitter Timeline Harvestor
+   
+   Usage:
+   
+       %s <twitter account>[ <twitter account>...]
+
+   The script will save all the tweets in a separate file for each account with
+   the name <twitter account>-tweets.txt.
+   
+""" % (sys.argv[0], sys.argv[0])
+    sys.exit()
+
 
 api = twitter.Api(consumer_key=os.environ.get('TWITTER_CONSUMER_KEY'),
                   consumer_secret=os.environ.get('TWITTER_CONSUMER_SECRET'),
                   access_token_key=os.environ.get('TWITTER_ACCESS_TOKEN_KEY'),
                   access_token_secret=os.environ.get('TWITTER_ACCESS_TOKEN_SECRET'))
 
-try:
-    lastid = os.readlink(LASTID_FILE)
-except:
-    lastid = 0
 
-print "Last ID: ", lastid
-
-statuses = api.GetUserTimeline(screen_name='@kf', since_id=int(lastid), count=200)
-
-corpusfile = open(CORPUS_FILE, "a")
-
-for status in statuses:
-    print "id: %s  msg: %s" % (status.id, status.text)
-    corpusfile.write(status.text.encode("UTF-8") + "\n")
+for account in sys.argv[1:]:
+    lastid_file = LASTID_FILE + '-' + account
     
-    if int(status.id) > int(lastid):
-        lastid = status.id
+    try:
+        lastid = os.readlink(lastid_file)
+    except:
+        lastid = 0
+    
+    print "Last ID for %s: %r" % (account, lastid)
 
-corpusfile.close()
+    statuses = api.GetUserTimeline(screen_name='@' + account, since_id=int(lastid), count=200)
 
-print "Last ID: ", lastid
-if os.path.islink(LASTID_FILE):
-    os.unlink(LASTID_FILE)
-os.symlink(str(lastid), LASTID_FILE)
-
-
+    corpusfile = open(account + '-tweets.txt', "a")
+    
+    for status in statuses:
+        print "[%s] id: %s  msg: %s" % (account, status.id, status.text)
+        corpusfile.write(status.text.encode("UTF-8") + "\n")
+        
+        if int(status.id) > int(lastid):
+            lastid = status.id
+    
+    corpusfile.close()
+    
+    print "Last ID: ", lastid
+    if os.path.islink(lastid_file):
+        os.unlink(lastid_file)
+    os.symlink(str(lastid), lastid_file)
+    
+    
--- kfcdbot.py
+++ kfcdbot.py
@@ -1,31 +1,50 @@
 #!/usr/bin/env python
 
 import sys
+import os
 from random import choice
 import twitter
 
-CORPUS_FILE="kftweets.txt"
+
+STARTKEY='STARTPAIR'
 
 
-api = twitter.Api(consumer_key=os.environ.get('TWITTER_CONSUMER_KEY'),
-                  consumer_secret=os.environ.get('TWITTER_CONSUMER_SECRET'),
-                  access_token_key=os.environ.get('TWITTER_ACCESS_TOKEN_KEY'),
-                  access_token_secret=os.environ.get('TWITTER_ACCESS_TOKEN_SECRET'))
+#api = twitter.Api(consumer_key=os.environ.get('TWITTER_CONSUMER_KEY'),
+#                  consumer_secret=os.environ.get('TWITTER_CONSUMER_SECRET'),
+#                  access_token_key=os.environ.get('TWITTER_ACCESS_TOKEN_KEY'),
+#                  access_token_secret=os.environ.get('TWITTER_ACCESS_TOKEN_SECRET'))
 
 
-def make_chains(corpus):
+def make_chains(wordfiles):
     """Takes an input text as a string and returns a dictionary of
     markov chains."""
     
     chain = {}
-    words = corpus.split()
+    chain[STARTKEY] = []
     
-    for i in range(len(words)-2):
-        pair = (words[i], words[i+1])
-        if (pair in chain):
-            chain[pair] += [ words[i+2] ]
-        else:
-            chain[pair] = [ words[i+2] ]
+    for filename in wordfiles:
+        try:
+            wordfile = open(filename, "r")
+        
+            line = wordfile.readline()
+            while line != '':
+                words = line.split()
+                
+                for i in range(len(words)-2):
+                    pair = (words[i], words[i+1])
+                    if i == 0:
+                        chain[STARTKEY].append(pair)
+                        
+                    if (pair in chain):
+                        chain[pair] += [ words[i+2] ]
+                    else:
+                        chain[pair] = [ words[i+2] ]
+                
+                line = wordfile.readline()
+            
+            wordfile.close()
+        except:
+            print "Unable to load file: ", filename
         
     return chain
 
@@ -34,15 +53,21 @@ def make_text(chains):
     based off an original text."""
     
     # To start, we want a word that starts with a capital letter
-    start = 'z'
-    while (start[0][0] != start[0][0].upper()):
-        start = choice(chains.keys())
+#    start = 'z'
+#    while (start[0][0] != start[0][0].upper()):
+#        start = choice(chains.keys())
+    
+    # Select a random pair from the start list
+    if len(chains[STARTKEY]) < 1:
+        sys.exit("Empty Chain Dictionary!")
+    
+    start = choice(chains[STARTKEY])
     
     line = list(start)
     
     last = line[-1][-1]
 #    while (len(line) < 10):
-    while (not line[-1][-1] in ['.','?']):
+    while (not line[-1][-1] in ['.','?'] and tuple(line[-2:]) in chains):
     	next = chains[tuple(line[-2:])]
     	line += [ choice(next) ]
 #    	print "line: %r" % line
@@ -54,11 +79,24 @@ def make_text(chains):
 
 def main():
     args = sys.argv
-    #filename = args[1]
 
-    # Change this to read input_text from a file
-    f = open(CORPUS_FILE, "r")
-    chain_dict = make_chains(f.read())
+    if len(args) < 2:
+        print """
+   %s - Python Markov Twitter Bot
+   
+   Usage:
+   
+       %s <corpse file>[ <corpusfile>...]
+
+   This script read in corpus files passed as command-line arguments and
+   generate a randomized tweet using markov chains.
+   
+""" % (sys.argv[0], sys.argv[0])
+        sys.exit()
+
+
+    # Create the markov chain dictionary
+    chain_dict = make_chains(args[1:])
 
     print make_text(chain_dict)
             
